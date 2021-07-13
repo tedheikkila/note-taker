387297eb9dfcaeafeb983547800865ad9015eea7
Ted Heikkila <ted.heikkila@gmail.com>
2021-07-12 19:37:01 -0500

    configuring delete op

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,18 +9,41 @@ module.exports = (app) => {
   app.get('/api/notes', (req, res) => res.json(notesData));
 
   // API posts; handles when a user submits a form and thus submits data to the server
-  // need a specific npm package to give each of these pushes a unique id
+  // need a specific npm package to give each push a unique id and push to db.json file
   app.post('/api/notes', (req, res) => {
-  
+    if (notesData.length < 100) {
       notesData.push(req.body);
-    
+      res.json(true);
+
+
+
+
+    } else {
+      res.json(false);
+    }
   });
+
 
   // delete data (do last)
   app.delete('/api/notes/:id', (req, res) => {
     // empties out the id's content (reads all db, remove id, rewrite to db file)
+    // something like notesData.id.length = 0 for end product
     notesData.length = 0;
 
     res.json({ ok: true });
   });
-};
+}
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
